47bed2e702a666908c6f0d4feb9cc89292ddf0ce
MaxusGuitar <maxim1999chikhanov@gmail.com>
2022-03-12 17:23:11 +0200

    The end!

--- src/App.js
+++ src/App.js
@@ -2,26 +2,13 @@ import { useState, useEffect } from "react";
 import ContactList from "./ContactList";
 import ContactForm from "./ContactForm";
 import FindContact from "./FindContact";
+import { useLocaleStorage } from "./hocks/useLocaleStorage";
 import shortid from "shortid";
 import style from "./App.css";
 
 export default function App() {
-  const [contacts, setContacts] = useState(() => {
-    const local = localStorage.getItem("contacts");
-    if (local) {
-      return JSON.parse(local);
-    } else {
-      return contactsArray;
-    }
-  });
+  const [contacts, setContacts] = useLocaleStorage("contacts", []);
   const [filter, setFilter] = useState("");
-
-  const contactsArray = [
-    { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-    { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-    { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-    { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-  ];
 
   const deleteContact = (contactid) => {
     setContacts((contacts) => contacts.filter((c) => c.id !== contactid));
